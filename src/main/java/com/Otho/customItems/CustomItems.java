7d93a3df72ad7a2e07f984784a4d13790a1ef8c6
Otho <marcelolotufo@hotmail.com>
2014-12-04 17:58:43 -0200

    Changing version from 1.0 -beta4 to 1.0.4 beta

--- src/main/java/com/Otho/customItems/CustomItems.java
+++ src/main/java/com/Otho/customItems/CustomItems.java
@@ -11,6 +11,7 @@ import com.Otho.customItems.lib.ModReference;
 import com.Otho.customItems.mod.GUI.GuiHandler;
 import com.Otho.customItems.mod.blocks.CustomChest;
 import com.Otho.customItems.mod.creativeTab.customItemsTab;
+
 import com.Otho.customItems.mod.tileentitys.TileEntityCustomChest;
 import com.Otho.customItems.proxy.ServerProxy;
 import com.Otho.customItems.util.LogHelper;
@@ -26,12 +27,6 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraft.client.audio.SoundRegistry;
 import net.minecraftforge.common.IExtendedEntityProperties;
-
-
-
-
-
-
 
 @Mod(dependencies=ModReference.DEPENDENCIES, modid  = ModReference.MOD_ID, version = ModReference.VERSION, name=ModReference.MOD_NAME)
 public class CustomItems
@@ -57,76 +52,13 @@ public class CustomItems
 		
 //		if(ModReference.makeRP)
 //		{
-//			boolean makeRPFolder = false;
-//			boolean makeCIFolder = false;
-//			
-//			File myFile = event.getSuggestedConfigurationFile();
-//			File configFolder = myFile.getParentFile();
-//			File mineFolder = configFolder.getParentFile();
-//			File RPFolder = new File(mineFolder.getPath()+ File.separator + "resourcepacks");
-//			
-//			File CIfolder = myFile;
-//			File meta;
-//			
-//			if(RPFolder.exists())
-//			{
-//				LogHelper.info("RPFolder already exist");
-//				CIfolder = new File(RPFolder.getPath()+ File.separator + "CustomItemsPack");
-//				if(!CIfolder.exists())
-//				{
-//					LogHelper.info("CIFolder does not exist");
-//					makeCIFolder = true;
-//				}else
-//				{
-//					LogHelper.info("CIFolder already exist");					
-//				}
-//			}else
-//			{
-//				LogHelper.info("RPFolder does not exist");
-//				makeRPFolder = true;
-//				makeCIFolder = true;
-//			}
-//			
-//			if(makeRPFolder)
-//			{
-//				RPFolder.mkdir();
-//			}
-//			
-//			if(makeCIFolder)
-//			{
-//				CIfolder.mkdir();
-//				meta = new File(CIfolder.getPath() + File.separator + "pack.mcmeta");
-//				
-//				meta.createNewFile();
-//				PrintWriter out = new PrintWriter(meta);
-//				out.println('{');
-//				out.println("\t"+'"'+"pack" +'"'+ ": {");
-//				out.println("\t\t"+'"'+"pack_format" +'"'+ ": 1,");
-//				out.println("\t\t"+'"'+"description" +'"'+ ": "+'"'+"Custom Items Pack"+'"');
-//				out.println("\t}");
-//				out.println("}");
-//				
-//				out.close();
-//				
-//				File assets = new File(CIfolder.getPath() + File.separator + "assets");
-//				assets.mkdir();
-//				File modId = new File(assets.getPath() + File.separator + "customitems");
-//				modId.mkdir();
-//				File textures = new File(modId.getPath() + File.separator + "textures");
-//				textures.mkdir();
-//				
-//				File blocks = new File(textures.getPath() + File.separator + "blocks");
-//				blocks.mkdir();
-//				File gui = new File(textures.getPath() + File.separator + "gui");
-//				gui.mkdir();
-//				File items = new File(textures.getPath() + File.separator + "items");
-//				items.mkdir();
-//				File models = new File(textures.getPath() + File.separator + "models");
-//				models.mkdir();
-//			}
+//			generateResourcePack(event.getSuggestedConfigurationFile());
 //		}
 		
 		JsonConfigurationHandler.init(folderPath);
+		
+		
+		
 		
     	proxy.registerTileEntities();
 	}	
@@ -140,5 +72,75 @@ public class CustomItems
     public void postInit(FMLPostInitializationEvent event)
     {
     	JsonConfigurationHandler.post_init();
+    }
+    
+    private void generateResourcePack (File myFile) throws IOException
+    {
+    	boolean makeRPFolder = false;
+		boolean makeCIFolder = false;		
+		
+		File configFolder = myFile.getParentFile();
+		File mineFolder = configFolder.getParentFile();
+		File RPFolder = new File(mineFolder.getPath()+ File.separator + "resourcepacks");
+		
+		File CIfolder = myFile;
+		File meta;
+		
+		if(RPFolder.exists())
+		{
+			LogHelper.info("RPFolder already exist");
+			CIfolder = new File(RPFolder.getPath()+ File.separator + "CustomItemsPack");
+			if(!CIfolder.exists())
+			{
+				LogHelper.info("CIFolder does not exist");
+				makeCIFolder = true;
+			}else
+			{
+				LogHelper.info("CIFolder already exist");					
+			}
+		}else
+		{
+			LogHelper.info("RPFolder does not exist");
+			makeRPFolder = true;
+			makeCIFolder = true;
+		}
+		
+		if(makeRPFolder)
+		{
+			RPFolder.mkdir();
+		}
+		
+		if(makeCIFolder)
+		{
+			CIfolder.mkdir();
+			meta = new File(CIfolder.getPath() + File.separator + "pack.mcmeta");
+			
+			meta.createNewFile();
+			PrintWriter out = new PrintWriter(meta);
+			out.println('{');
+			out.println("\t"+'"'+"pack" +'"'+ ": {");
+			out.println("\t\t"+'"'+"pack_format" +'"'+ ": 1,");
+			out.println("\t\t"+'"'+"description" +'"'+ ": "+'"'+"Custom Items Pack"+'"');
+			out.println("\t}");
+			out.println("}");
+			
+			out.close();
+			
+			File assets = new File(CIfolder.getPath() + File.separator + "assets");
+			assets.mkdir();
+			File modId = new File(assets.getPath() + File.separator + "customitems");
+			modId.mkdir();
+			File textures = new File(modId.getPath() + File.separator + "textures");
+			textures.mkdir();
+			
+			File blocks = new File(textures.getPath() + File.separator + "blocks");
+			blocks.mkdir();
+			File gui = new File(textures.getPath() + File.separator + "gui");
+			gui.mkdir();
+			File items = new File(textures.getPath() + File.separator + "items");
+			items.mkdir();
+			File models = new File(textures.getPath() + File.separator + "models");
+			models.mkdir();
+		}
     }
 }
--- src/main/java/com/Otho/customItems/lib/ModReference.java
+++ src/main/java/com/Otho/customItems/lib/ModReference.java
@@ -3,7 +3,7 @@ package com.Otho.customItems.lib;
 public class ModReference {
 	public static final String MOD_ID = "customitems";
 	public static final String MOD_NAME = "Custom Items";
-	public static final String DEPENDENCIES = "after:all"; 
+	public static final String DEPENDENCIES = ""; 
 	public static final String VERSION = "1.0";
 	public static boolean debug = true;
 	public static boolean makeRP = true;	
