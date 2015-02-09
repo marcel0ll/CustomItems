d4dd9bb319e92999257e03aa2e23f1428294ef51
Otho <marcelolotufo@hotmail.com>
2015-02-09 13:50:04 -0200

    Addin folder for config files
    I will put in this folder mainly default config files and test files

--- src/main/java/me/otho/customItems/configuration/JsonSchema.java
+++ src/main/java/me/otho/customItems/configuration/JsonSchema.java
@@ -25,21 +25,24 @@ import me.otho.customItems.configuration.jsonReaders.worldGen.Cfg_oreGen;
 
 public class JsonSchema 
 {	
-	public Cfg_block[] blocks;
-	public Cfg_chest[] chests;
-	public Cfg_item[] items;	
+	public Cfg_block[] blocks;	
+	public Cfg_chest[] chests;	
+	public Cfg_item[] items;		
 	public Cfg_food[] foods;
-	public Cfg_pickaxe[] pickaxes;
+	
+	public Cfg_pickaxe[] pickaxes;	
 	public Cfg_axe[] axes;
 	public Cfg_shovel[] shovels;
 	public Cfg_hoe[] hoes;
 	public Cfg_sword[] swords;
+	
 	public Cfg_helmet[] helmets;
 	public Cfg_chestplate[] chestplates;
 	public Cfg_leggings[] leggings;
 	public Cfg_boots[] boots;
-	public Cfg_fluid[] fluids;
-	public Cfg_creativeTab[] creativeTabs;
+	
+	public Cfg_fluid[] fluids;	
+	public Cfg_creativeTab[] creativeTabs;	
 	public Cfg_crop[] crops;
 	
 	public Cfg_change_block[] changeBlocks;
