0a328af79f340c794507930d3656661bdc5efe36
MaPePeR <MaPePeR@users.noreply.github.com>
2015-09-22 21:58:02 +0200

    Register custom Ex Nihilo Hammers

---
+++ src/main/java/me/otho/customItems/mod/items/tools/CustomHammer.java
@@ -0,0 +1,9 @@
+package me.otho.customItems.mod.items.tools;
+
+import exnihilo.items.hammers.ItemHammerBase;
+
+public class CustomHammer extends ItemHammerBase{
+	public CustomHammer(ToolMaterial material){
+		super(material);
+	}
+}
